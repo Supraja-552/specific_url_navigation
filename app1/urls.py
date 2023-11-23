4713d7103c97dc8895faa928a9301186c12e18bb
Supraja <chimmanisupraja@gmail.com>
2023-11-23 19:33:57 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,4 @@
+from app1.views import*
+from django.urls import path
+app_name='app1'
+urlpatterns=[path('first/',first,name="first"),]
---
+++ app2/urls.py
@@ -0,0 +1,5 @@
+from app2.views import *
+from django.urls import path
+app_name='app2'
+urlpatterns=[
+    path('second/',second,name='second'),]
